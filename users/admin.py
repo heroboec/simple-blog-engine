7cb47f808e5cc009c7ca31ec90cbc99f6694a689
bip <help@buyinstaprom.com>
2021-03-24 18:36:55 +0000

    users app: create

--- users/admin.py
+++ users/admin.py
@@ -1,15 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+
 # Register your models here.
-
-
-from .forms import CustomUserCreationForm
-from .forms import CustomUserChangeForm
-from .models import CustomUser
-
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-
-admin.site.register(CustomUser, CustomUserAdmin)
