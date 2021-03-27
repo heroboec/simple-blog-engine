3b8e0d656f36ececc902521633ec4dd0635c5277
Denis <zealot351@gmail.com>
2021-03-27 21:44:12 +0300

    users.urls.py: add path

--- users/urls.py
+++ users/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic.base import TemplateView
+from .views import SignupView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('signup/', SignupView.as_view(), name='signup'),
 ]
